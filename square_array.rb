dd6b1d9fab72a721165aa8efce4fa28af1457073
Denise Pendleton <dpendl1@hotmail.com>
2018-02-20 04:25:48 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
 
   array.each do |element|
     new_array = []
-    new_element = element ** 2
-    new_array << new_element
+    # new_element = element ** 2
+    new_array << (element**2)
   end
   end
